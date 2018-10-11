1277aaa92e0b925d66a893615258e08e6d4d4cc3
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-10-10 20:42:23 -0400

    Done.

--- src/Tier2.js
+++ src/Tier2.js
@@ -13,12 +13,11 @@ export default class Tier2 extends Component {
   }
 
   render() {
-    // hard coded color values have been added below, though they won't be
-    // present in our solution. What should they be replaced with?
+
     return (
-      <div className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
-        <Tier3 color={"#0F0"} />
-        <Tier3 color={"#F00"} />
+      <div className="tier2" onClick={props.handleClick} style={{backgroundColor: this.props.color, color: this.props.color}}>
+        <Tier3 handleClick={props.handleChildClick} color={props.childColor} />
+        <Tier3 handleClick={props.handleChildClick} color={props.childColor} />
       </div>
     )
   }
